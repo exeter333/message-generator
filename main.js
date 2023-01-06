c81b50f1a5588d54ef44c95d6bc9c92e22b24e94
Jose <joseluizsccavalcanti@gmail.com>
2023-01-06 15:19:28 -0500

    Add variables with arrays

---
+++ main.js
@@ -0,0 +1,3 @@
+const sign = ['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio','sagittarius','capricorn','aquarius','pisces'];
+const date = ['today', 'tomorrow', 'this month', 'this year', 'this week', 'next week', 'next month'];
+const message = ['you will find good luck', 'love will come', 'bad luck shall find you', 'work and finances will ask more from you', 'will be more chaotic than usual', 'will bring moresurprises than usual'];
