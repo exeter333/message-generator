b0b8afb830f98467a0fa1f36197e7a04076430d7
Jose <joseluizsccavalcanti@gmail.com>
2023-01-07 02:25:55 -0500

    Delete 'final' variable and add method

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const sign = ['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio','sagittarius','capricorn','aquarius','pisces'];
 const date = ['today', 'tomorrow', 'this month', 'this year', 'this week', 'next week', 'next month'];
 const message = ['you will find good luck', 'love will come', 'bad luck shall find you', 'work and finances will ask more from you', 'will be more chaotic than usual', 'will bring more surprises than usual'];
-let final = '';
 const buildMessage = (arrSign, arrDate, arrMes) => {
-    
+    return `${sign[Math.floor(Math.random()*(sign.length-1))]} + ${date[Math.floor(Math.random()*(date.length-1))]} ${message[Math.floor(Math.random()*(message.length-1))]}`;
 };
