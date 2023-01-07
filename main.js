781f74df13bc63a54f3f88b6bc96b8ad471c7e08
Jose <joseluizsccavalcanti@gmail.com>
2023-01-07 02:31:36 -0500

    Correct variables in method

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const sign = ['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio'
 const date = ['today', 'tomorrow', 'this month', 'this year', 'this week', 'next week', 'next month'];
 const message = ['you will find good luck', 'love will come', 'bad luck shall find you', 'work and finances will ask more from you', 'will be more chaotic than usual', 'will bring more surprises than usual'];
 const buildMessage = (arrSign, arrDate, arrMes) => {
-    return `${sign[Math.floor(Math.random()*(sign.length-1))]} + ${date[Math.floor(Math.random()*(date.length-1))]} ${message[Math.floor(Math.random()*(message.length-1))]}`;
+    return `${arrSign[Math.floor(Math.random()*(arrSign.length-1))]} + ${arrDate[Math.floor(Math.random()*(arrDate.length-1))]} ${arrMes[Math.floor(Math.random()*(arrMes.length-1))]}`;
 };
+console.log(buildMessage(sign, date, message));
