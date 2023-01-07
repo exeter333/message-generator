d862988ae68afeeb62c99d54e408c9c31a985aca
Jose <joseluizsccavalcanti@gmail.com>
2023-01-07 02:37:32 -0500

    Rewrite function to increase readability

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const sign = ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio'
 const date = ['today', 'tomorrow', 'this month', 'this year', 'this week', 'next week', 'next month'];
 const message = ['you will find good luck', 'love will come', 'bad luck shall find you', 'work and finances will ask more from you', 'will be more chaotic than usual', 'will bring more surprises than usual'];
 const buildMessage = (arrSign, arrDate, arrMes) => {
-    return `${arrSign[Math.floor(Math.random()*(arrSign.length-1))]}: ${arrDate[Math.floor(Math.random()*(arrDate.length-1))]} ${arrMes[Math.floor(Math.random()*(arrMes.length-1))]}`;
+    const randSign = arrSign[Math.floor(Math.random()*(arrSign.length-1))];
+    const randDate = arrDate[Math.floor(Math.random()*(arrDate.length-1))];
+    const randMes = arrMes[Math.floor(Math.random()*(arrMes.length-1))]
+    return `${randSign}: ${randDate} ${randMes}`;
 };
 console.log(buildMessage(sign, date, message));
